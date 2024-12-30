9a62eaeaf7177e678bc7832751bd561aecc9d436
mukesh4215 <kandhimallamukesh@gmail.com>
2024-12-30 11:35:43 +0530

    js

---
+++ index.js
@@ -0,0 +1,39 @@
+// console.log("i am external")
+// var a;
+// a=10;
+// console.log(a);
+// var b=20;
+// console.log(b);
+// a=30;
+// console.log(a);
+// var a=100;
+// console.log(a);
+
+// let a;
+// a=10;
+// console.log(a);
+// let b=20;
+// console.log(b);
+// a=30;
+// console.log(a);
+// let a=200;
+
+// const a="hii";
+// console.log(a);
+
+var sname=`ben`;
+var sgrade='A';
+var sage=16;
+console.log(sname,sgrade,sage);
+var ename="jacob";
+var esal=30000.25;
+var edept='science';
+console.log("employename="+ename,esal,edept);
+
+
+
+
+
+
+
+
