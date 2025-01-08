aad3bb59cfd61fe2e0f6c788c0d44070f567c4cb
mukesh4215 <kandhimallamukesh@gmail.com>
2025-01-08 15:36:56 +0530

    java script

--- index.js
+++ index.js
@@ -21,14 +21,150 @@
 // const a="hii";
 // console.log(a);
 
-var sname=`ben`;
-var sgrade='A';
-var sage=16;
-console.log(sname,sgrade,sage);
-var ename="jacob";
-var esal=30000.25;
-var edept='science';
-console.log("employename="+ename,esal,edept);
+// var sname=`ben`;
+// var sgrade='A';
+// var sage=16;
+// console.log(sname,sgrade,sage);
+// var ename="jacob";
+// var esal=30000.25;
+// var edept='science';
+// console.log("employename="+ename,esal,edept);
+
+// var mobname=`opple`;
+// var cost=100000;
+// var color='black';
+// console.log("mobile name="+mobname,"mobile cost="+cost,"mobile color="+color);
+
+// var carname="benz";
+// var cost=30000.25;
+// var type='P';
+// console.log("car name="+carname,"car cost="+cost,"type="+type);
+
+// var sname="rms";
+// var strength=3000;
+// var sgrade='A';
+// console.log("school name="+sname,"school strength="+strength,"school grade="+sgrade);
+
+// let tvname="sony";
+// let tvcost=30000;
+// let tvtype='A';
+// console.log("tv name="+tvname,"tv cost="+tvcost,"tv type="+tvtype);
+
+// let bikename="diooo";
+// let bikecost=20000;
+// let bikecolor='black';
+// console.log("bike name="+bikename,"bike cost="+bikecost,"bike color="+bikecolor);
+
+// let univname="cmr";
+// let courname="data science";
+// let examcost=2000;
+// console.log("univ name="+univname,"course name="+courname,"exam cost="+examcost);
+
+// const lapname="dell";
+// const lapcost=100000;
+// const lapbrand='xps';
+// console.log("laptop name="+lapname,"laptop cost="+lapcost,"laptop brand="+lapbrand);
+
+// const hname="nilayaa";
+// const hcost=1000000;
+// const hcolor='blue';
+// console.log("house name="+hname,"house cost="+hcost,"house color="+hcolor);
+
+
+/*scope of variable*/
+// {
+//     var a=10;
+//     let b=20;
+//     const c=30;
+//     console.log(a,b,c);
+// }
+// function add(){
+//     var a=10;
+//     let b=20;
+//     const c=30;
+//     console.log(a+b+c);
+// }
+// add();
+// var a=10;
+// let b=20;
+// const c=30;
+// function add(){
+//     console.log(a+b+c);
+// }add();
+
+// function mul(){
+//     console.log(a*b*c);
+// }mul();
+
+//datatypes
+
+// var a=10;
+// var b=20.23;
+// console.log(parseFloat(a+b));
+
+// var a=10.236;
+// console.log(a.toFixed(2));
+
+// var a=20;
+// console.log(Number.isInteger(a));
+// let b=15/5;
+// console.log(Number.isFinite(b));
+
+// let str ="java";
+// console.log(str.charCodeAt(0));
+
+// let str1 ="jspiders";
+// // console.log(str1.slice(0,5));
+// console.log(str1.slice(-8,-2));
+
+// let str3="jspiders";
+// console.log(str3.substr(0,6));
+
+// let str4="jspiders";
+// console.log(str4.substring(0,5));
+
+// let str5="     javascript     ";
+// console.log(str5.length);
+// let a=str5.trim();
+// console.log(a.length);
+
+// let str6="java";
+// let str7="script";
+// let b=str6.concat(str7);
+// console.log(b);
+
+// let str='jspiders';
+// console.log(str.replace('j','a'));
+// console.log(str.replaceAll('s','e'));
+
+// let str='rajajinagar';
+// console.log(str.includes('z'));
+// console.log(str.includes('i'));
+
+// let str='doddles';
+// console.log(str.indexOf('d'));
+// console.log(str.lastIndexOf('d'));
+
+// let str='5';
+// console.log(str.padStart(3,0));
+// console.log(str.padEnd(3,0));
+
+// let str='jspider';
+// console.log(str.startsWith('h'));
+// console.log(str.endsWith('r'));
+
+// let str='aurora';
+// console.log(str.search('u'));
+
+let str='yokohoma apollo';
+let a=str.split("   ");
+console.log(a);
+
+
+
+
+
+
 
 
 
